a56c036565a42eaf5a9706e261bc60a7ab57db75
Zde <zde@zde.zde>
2019-10-12 17:22:49 +0200

    Move code to "main" function

--- round-one.py
+++ round-one.py
@@ -1,17 +1,23 @@
-user = input('Enter you name: ')
-password = input('Enter your password must be 10 word and number: ')
-limit = 10
-limit2 = 6
-count = 0
-while len(password) != limit :
-    print('Your maximum is only 10 not less or more')
-    count = count +1
-    if count >= 3:
-        quit()
-else:
-    print('Access your account')
-access = input("Enter your code: ")
-if len(access) != limit2:
-    print('Please enter right code')
-else:
-    print('done')
+#!/usr/bin/python3
+
+def main():
+  user = input('Enter you name: ')
+  password = input('Enter your password must be 10 word and number: ')
+  limit = 10
+  limit2 = 6
+  count = 0
+  while len(password) != limit :
+      print('Your maximum is only 10 not less or more')
+      count = count +1
+      if count >= 3:
+          quit()
+  else:
+      print('Access your account')
+  access = input("Enter your code: ")
+  if len(access) != limit2:
+      print('Please enter right code')
+  else:
+      print('done')
+
+if __name__ == "__main__":
+    main()
