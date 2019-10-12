447c16a60c2ea1c60933d9ec055b741f1832187a
Zde <zde@zde.zde>
2019-10-12 17:58:56 +0200

    Make hints more helpful

--- round-one.py
+++ round-one.py
@@ -12,9 +12,18 @@ class Game:
             int_guess = int(guess)
         except ValueError:
             return False
+        return self.verify_number(int_guess)
+
+    def verify_number(self, int_guess):
         return int_guess == 1234
 
     def hint(self, guess):
+        if guess is None:
+            return 'Guess the number!'
+        try:
+            int(guess)
+        except ValueError:
+            return 'Enter the number'
         return 'Enter 1234 to win!'
 
 
